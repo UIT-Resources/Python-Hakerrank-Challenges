109912619bc394c39913bf139187e01cd45c4505
Ad_DoanNgocTai <doanngoctai1998@gmail.com>
2019-08-09 14:06:54 +0700

    Init Repository
    + Add String Challenge :
      - String Formatting
      - String Validator
      - Text alignment

---
+++ String Challenges/String Validator.py
@@ -0,0 +1,47 @@
+if __name__ == '__main__':
+    s = input()
+    hasAnyAlnum = False
+    hasAnyAlpha = False
+    hasAnyDigit = False
+    if (s.isalnum()):
+        hasAnyAlnum = True
+        if (s.isdigit() == False):
+            hasAnyAlpha = True
+        else:
+            hasAnyAlpha = False
+
+        if (s.isalpha() == False):
+            hasAnyDigit = True
+        else:
+            hasAnyDigit = False
+    else:
+        temp = s.upper()  # Tranfer to all upper case
+        for i in range(65, 91):  # A->Z
+            if (temp.find(chr(i)) != -1):
+                hasAnyAlnum = True
+                hasAnyAlpha = True
+                break
+        for i in range(48, 58):  # 0 -> 9
+            if (temp.find(chr(i)) != -1):
+                hasAnyAlnum = True
+                hasAnyDigit = True
+                break
+    print(hasAnyAlnum)
+    print(hasAnyAlpha)
+    print(hasAnyDigit)
+
+    if (hasAnyAlpha):
+        if (s.isupper() == False):
+            print("True")
+        else:
+            print("False")
+
+        if (s.islower() == False):
+            print("True")
+        else:
+            print("False")
+    else:
+
+        print("False")
+        print("False")
+
